e2505321973e850471661f10f906822403b1aeee
957273408 <957273408@qq.com>
2019-01-23 18:54:33 +0800

    配装city路由搭建city页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/Home'
+import Home from '@/pages/home/Home'
+import { resolve } from 'url';
+
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: "/city",
+      name: "city",
+      component: resolve => require(['@/pages/city/city'],resolve)
     }
   ]
 })
